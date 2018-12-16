e26d4b279b865a4471b3f9d0d249c5af556024e5
papaponmx <jaimemadrigalrios@gmail.com>
2018-12-16 00:52:11 -0600

    Added env variables

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import Helmet from 'react-helmet';
 import styled from 'react-emotion';
 import { Header } from 'components';
 import { Layout } from 'layouts';
-import GithubIcon from '../svg/github'
+import GithubIcon from '../svg/github';
 
 export const Button = styled.a`
   background: white;
@@ -11,33 +11,38 @@ export const Button = styled.a`
   color: #555;
   border-radius: 4px;
   padding: 8px 12px;
-  
+
   &:hover {
     background: #fcfcfc;
     box-shadow: 0 2px #666;
     cursor: pointer;
   }
-  `;
+`;
 
 export const Icon = styled.span`
-    position: relative;
-    top: 6px;
-    `;
+  position: relative;
+  top: 6px;
+`;
 
 const Index = () => {
+  const { GITHUB_TURRIS_CLIENT_ID } = process.env;
   return (
     <Layout>
       <Helmet title={'Turris CI'} />
       <Header title="Turris CI">
-        <Button>
-        <Icon>
-          <GithubIcon />
-        </Icon>
-        Log in with Github</Button>
+        <Button
+          target="_blank"
+          rel="noopener noreferrer"
+          href={`https://github.com/login/oauth/authorize?client_id=${GITHUB_TURRIS_CLIENT_ID}`}
+        >
+          <Icon>
+            <GithubIcon />
+          </Icon>
+          Log in with Github
+        </Button>
       </Header>
     </Layout>
   );
 };
 
 export default Index;
-
